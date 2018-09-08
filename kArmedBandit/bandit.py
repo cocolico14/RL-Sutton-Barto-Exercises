cd0d1c4d207d80251d75c0899e549e561bed02e7
Soheil <cocolico12@gmail.com>
2018-09-08 13:42:53 +0430

    optimize code

--- kArmedBandit/bandit.py
+++ kArmedBandit/bandit.py
@@ -1,20 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from statistics import Stats
-import sys, os
-
-def blockPrint():
-    sys.stdout = open(os.devnull, 'w')
-
-def enablePrint():
-    sys.stdout = sys.__stdout__
+from math import pow, e
 
 class EpsilonGreedy():
 
     def __init__(self, armCount, epsilon=0.1, alpha = None):
         self.values = np.zeros(armCount)
         self.pullCount = np.zeros(armCount)
+        self.choice = np.arange(armCount)
         self.armCount = armCount
         self.epsilon = epsilon
         self.alpha = alpha
@@ -26,7 +20,7 @@ class EpsilonGreedy():
         if np.random.rand() > self.epsilon: # Exploit
             return np.argmax(self.values)
         else: # Explore
-            return int(np.floor(np.random.uniform(high=self.armCount)))
+            return np.random.choice(self.choice)
 
     def update(self, i, reward):
         self.pullCount[i] += 1
@@ -44,121 +38,93 @@ class Softmax():
     def __init__(self, armCount, temp=0.1):
         self.values = np.zeros(armCount)
         self.pullCount = np.zeros(armCount)
-        self.armCount = armCount
-        self.choice = np.array(list(range(armCount)))
         self.temp = temp
+        self.armCount = armCount
+        self.valueExp = np.ones(armCount)
+        self.sumExp = 1 * self.armCount
+        self.prob = self.valueExp / self.armCount 
+        self.choice = np.arange(armCount)
+        
 
     def __repr__(self):
         return "Softmax: " + str(self.temp)
 
     def selectArm(self):
-        probability = np.exp(self.values/self.temp)
-        sumExp = np.sum(probability)
-        probability /= sumExp
-        return np.random.choice(self.choice, p=probability)
+        return np.random.choice(self.choice, p=self.prob)
 
     def update(self, i, reward):
         self.pullCount[i] += 1
         count = self.pullCount[i]
         value = self.values[i]
-        prob = np.exp(value/self.temp)/np.sum(np.exp(self.values/self.temp))
-        self.values[i] = value + (1/count) * (reward - value) * (1 - prob)
+        self.sumExp -= self.valueExp[i]
+        self.values[i] = value + (1/count) * (reward - value) #* (1 - prob)
+        m = self.values[i] / self.temp
+        self.valueExp[i] = pow(e,m)
+        self.sumExp += self.valueExp[i]
+        self.prob = self.valueExp / self.sumExp
         
 
     def refresh(self):
         self.values.fill(0)
         self.pullCount.fill(0)
-
-class Arm():
-
-    def __init__(self):
-        self.prob = np.random.rand()
-
-    def pull(self):
-        return 1 if np.random.rand()<self.prob else 0
-
-    def getOptimality(self):
-        return self.prob
-
-    def __iter__(self):
-        while True:
-            yield Arm()
-
-    def __repr__(self):
-        rep = "{0:.2f}%".format(self.prob*100)
-        return rep
-
+        self.valueExp.fill(1.0)
+        self.sumExp = 1 * self.armCount
+        self.prob.fill(1/self.armCount)
 
 
 if __name__ == '__main__':
 
     armCount = 10
-    epochs = 1000
-    play = 2000
+    play = 1000
+    run = 2000
 
-    blockPrint()
-    param = [{'_algo': Softmax(armCount, 1), '_epochs': epochs,
-             '_optAct': None, '_reward': None, 
-             '_optActMean': None, '_rewardMean': None, '_color': 'blue'}
-             
-             ]
+    algos = [EpsilonGreedy(armCount, 0.5)]
+    colors = ['blue', 'red', 'green', 'magenta', 'cyan']
+    rewardData = np.zeros((len(algos), play, run))
+    optimalityData = np.zeros((len(algos), play, run))
 
-    cases = [Stats(**(param[i])) for i in range(len(param))]
-    enablePrint()
+    arms = np.zeros((run, armCount))
 
-    for p in tqdm(range(play)):
+    for r in range(run):
+        np.random.seed(r)
+        arms[r] = np.random.randn(armCount)
 
-        np.random.seed(p)
+    for r in (range(run)):
 
-        armGen = iter(Arm())
-        arms = [next(armGen) for i in range(armCount)]
-        cumulativeReward = 0
-        optimalChoice = np.argmax([arm.getOptimality() for arm in arms])
+        optimalChoice = np.argmax(arms[r])
 
-        for i in range(armCount):
-            reward = arms[i].pull()
-            for c in range(len(cases)):
-                cases[c].algorithm.update(i, reward)
-
-        for e in range(epochs):
-            for c in range(len(cases)):
-                choosen = cases[c].algorithm.selectArm()
-                reward = arms[choosen].pull()
-                cases[c].algorithm.update(choosen, reward)
-                cases[c].reward[e] += reward
+        for p in range(play):
+            for c in range(len(algos)):
+                choosen = algos[c].selectArm()
+                reward = np.random.rand()+arms[r][choosen]
+                algos[c].update(choosen, reward)
+                rewardData[c][p][r] += reward
                 if optimalChoice == choosen:
-                    cases[c].optimalAction[e] += 1
+                    optimalityData[c][p][r] += 1
         
-        for c in range(len(cases)):
-            cases[c].optimalActionMean += cases[c].optimalAction
-            cases[c].rewardMean += cases[c].reward
-            cases[c].optimalAction.fill(0)
-            cases[c].reward.fill(0)
-            cases[c].algorithm.refresh()
+        for c in range(len(algos)):
+            # pullCount = [int(c) for c in algos[c].pullCount]
+            # for (k, v) in zip(arms, pullCount):
+            #     print(str(k) + "% = " + str(v) + " pulls")
+            algos[c].refresh()
 
-        # pullCount = [int(c) for c in bandit.pullCount]
-        # for (k, v) in zip(arms, pullCount):
-        #     print(str(k) + " = " + str(v) + " pulls")
-        # print("\nTotal Reward: " + str(cumulativeReward))
+    rewardMean = np.mean(rewardData, axis=2)
+    optimalityMean = np.mean(optimalityData, axis=2)
+    time = np.array(list(range(play)))
 
-    
-    for c in range(len(cases)):
-        cases[c].rewardMean /= play
-        cases[c].optimalActionMean /= play
-        time = np.array(list(range(epochs)))
+    for c in range(len(algos)):
         plt.subplot(2, 1, 1)
-        plt.plot(time, cases[c].optimalActionMean, '-o'
-                , color=cases[c].color, markersize=0.7, lw=1.5, label=str(cases[c].algorithm))
+        plt.plot(time, optimalityMean[c], '-o'
+                , color=colors[c], markersize=0.7, lw=1.5, label=str(algos[c]))
         plt.ylabel('% Optimal Action')
 
         plt.subplot(2, 1, 2)
-        plt.plot(time, cases[c].rewardMean, '-o'
-                , color=cases[c].color, markersize=0.7, lw=1.5, label=str(cases[c].algorithm))
+        plt.plot(time, rewardMean[c], '-o'
+                , color=colors[c], markersize=0.7, lw=1.5, label=str(algos[c]))
         plt.ylabel('Average Reward')
-        plt.xlabel('Epochs')
+        plt.xlabel('play')
 
     plt.legend(loc='best')
-    plt.axis([0, epochs, 0, 1])
     plt.show()
         
 
